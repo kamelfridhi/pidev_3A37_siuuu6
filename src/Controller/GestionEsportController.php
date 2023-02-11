8f331e6c1807fee738b511c3ae660cc07672e78d
kamelfridhi <80178238+kamelfridhi@users.noreply.github.com>
2023-02-12 00:38:52 +0100

    up

--- src/Controller/GestionEsportController.php
+++ src/Controller/GestionEsportController.php
@@ -16,7 +16,7 @@ class GestionEsportController extends AbstractController
         ]);
     }
     /**
-     * @Route("/front", name="welcome_front")
+     * @Route("/front", name="home")
      */
     public function welcomefront(): Response
     {
