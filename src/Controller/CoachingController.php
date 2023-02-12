33682305b8d80bd3e7517cd03518caba74b265a2
medmehdihanini <80129416+medmehdihanini@users.noreply.github.com>
2023-02-12 02:02:18 +0100

    Update CoachingController.php

--- src/Controller/CoachingController.php
+++ src/Controller/CoachingController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CoacheController extends AbstractController
+class CoachingController extends AbstractController
 {
     #[Route('/coache', name: 'app_coache')]
     public function index(): Response
